bf8a7dc977b73c6563f6e50a339cf6e345a1d1a2
Samuel Gebrehiwet <samuelbruk@gmail.com>
2017-07-04 16:14:01 -0700

    add index, dashboard

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
-app.set('view engine', 'ejs');
+// let tweetHistory = [];
+
+app.set('view engine', 'ejs')
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+    const {tweetHistory} = req.cookies;
+    res.locals.username = tweetHistory;
+    next();
+  });
 
 app.get('/', function(req, res){
-  response.render('index');
+  res.render('index');
+});
+
+app.get('/dashboard', function(req, res){
+  let {tweetHistory} = (req.cookies.tweetHistory)? req.cookies : {tweetHistory: []};
+  res.render('dashboard', {tweetHistory:{tweetHistory}});
+});
+
+app.post('/dashboard', function(req, res){
+  const {userTweet} = req.body;
+  // Initialize tweetHistory with existing cookie content or an empty array
+  let {tweetHistory} = (req.cookies.tweetHistory)? req.cookies : {tweetHistory: []};
+  tweetHistory.push(userTweet);
+  res.cookie('tweetHistory', tweetHistory, {maxAge: 1000*60*60*24});
+  res.render('dashboard', {tweetHistory:{tweetHistory}})
 });
 
 const PORT = 5000;
